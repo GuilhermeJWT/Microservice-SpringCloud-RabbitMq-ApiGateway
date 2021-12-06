28345c164fbd0a8bb7d427faca3452b90b134fdb
GuilhermeJWT <guiromanno@gmail.com>
2021-12-06 18:07:33 -0300

    adiciona equals e hashcode

--- crudservice/src/main/java/br/com/systemsgs/crudservice/dto/ProdutoDTO.java
+++ crudservice/src/main/java/br/com/systemsgs/crudservice/dto/ProdutoDTO.java
@@ -13,5 +13,37 @@ public class ProdutoDTO implements Serializable{
 	private Integer estoque;
 	
 	private Double preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
 	
 }
