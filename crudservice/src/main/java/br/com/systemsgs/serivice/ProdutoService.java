c9390c2c6969ae7f99151900f751c5df2c4b874d
GuilhermeJWT <guiromanno@gmail.com>
2021-12-24 11:24:57 -0300

    Criando as demais operações

--- crudservice/src/main/java/br/com/systemsgs/serivice/ProdutoService.java
+++ crudservice/src/main/java/br/com/systemsgs/serivice/ProdutoService.java
@@ -1,10 +1,16 @@
 package br.com.systemsgs.serivice;
 
+import br.com.systemsgs.exception.ResourceNotFoundException;
+import br.com.systemsgs.model.ModelProduto;
 import br.com.systemsgs.repository.ProdutoRepository;
 import br.com.systemsgs.vo.ProdutoVO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -17,7 +23,39 @@ public class ProdutoService {
 
     @Transactional
     public ProdutoVO create(ProdutoVO produtoVO){
+        ProdutoVO produtoRetorno = ProdutoVO.converteEntidade(produtoRepository.save(ModelProduto.coverteEntidade(produtoVO)));
         return null;
+    }
+
+    public ProdutoVO findById(Long id){
+        var produto = produtoRepository.findById(id).orElseThrow(() -> new ResourceAccessException("Produto não Encontrado!"));
+
+        return ProdutoVO.converteEntidade(produto);
+    }
+
+    public ProdutoVO alterar(ProdutoVO produtoVO){
+        final Optional<ModelProduto> optionalPorduto = produtoRepository.findById(produtoVO.getId());
+
+        if (!optionalPorduto.isPresent()){
+            new ResourceNotFoundException("Produto não Encontrado!");
+        }
+
+        return ProdutoVO.converteEntidade(produtoRepository.save(ModelProduto.coverteEntidade(produtoVO)));
+    }
+
+    public void delete(Long id){
+        var produto = produtoRepository.findById(id).orElseThrow(() -> new ResourceAccessException("Produto não Encontrado!"));
+        produtoRepository.delete(produto);
+    }
+
+    public Page<ProdutoVO> findAll(Pageable pageable){
+        var page = produtoRepository.findAll(pageable);
+
+        return page.map(this::convertToProdutoVO);
+    }
+
+    private ProdutoVO convertToProdutoVO(ModelProduto modelProduto){
+        return ProdutoVO.converteEntidade(modelProduto);
     }
 
 }
