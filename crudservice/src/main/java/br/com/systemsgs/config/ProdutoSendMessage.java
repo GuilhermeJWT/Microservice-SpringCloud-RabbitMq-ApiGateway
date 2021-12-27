6452e14d53923c5197fcaa6afa7f73a7e90877fa
GuilhermeJWT <guiromanno@gmail.com>
2021-12-27 18:19:56 -0300

    Configuração do RabbitMq - Direct Exchange

---
+++ crudservice/src/main/java/br/com/systemsgs/config/ProdutoSendMessage.java
@@ -0,0 +1,29 @@
+package br.com.systemsgs.config;
+
+import br.com.systemsgs.vo.ProdutoVO;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoSendMessage {
+
+    @Value("${crud.rabbitmq.exchange}")
+    String exchange;
+
+    @Value("${crud.produto.routingKey}")
+    String routingKey;
+
+    public final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public ProdutoSendMessage(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(ProdutoVO produtoVO){
+        rabbitTemplate.convertAndSend(exchange, routingKey,produtoVO);
+    }
+
+}
