c3bd1cc1f53358ba95bcae0d15864c6a7538984d
GuilhermeJWT <guiromanno@gmail.com>
2021-12-28 21:28:20 -0300

    Criando o Model User e Permissions com relacionamento e Spring Security

---
+++ auth/src/main/java/br/com/systemsgs/model/ModelPermission.java
@@ -0,0 +1,29 @@
+package br.com.systemsgs.model;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "permission")
+public class ModelPermission implements GrantedAuthority, Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+}
---
+++ auth/src/main/java/br/com/systemsgs/model/ModelUser.java
@@ -0,0 +1,85 @@
+package br.com.systemsgs.model;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "user")
+public class ModelUser implements UserDetails,Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "accountNonExpired")
+    private Boolean accountNonExpired;
+
+    @Column(name = "accountNonLocked")
+    private Boolean accountNonLocked;
+
+    @Column(name = "credentialsNonExpired")
+    private Boolean credentialsNonExpired;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_permission", joinColumns = { @JoinColumn(name = "id_user")}, inverseJoinColumns = { @JoinColumn(name = "id_permissions")})
+    private List<ModelPermission> permissions;
+
+    public List<String> getRoles(){
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.permissions;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+}
