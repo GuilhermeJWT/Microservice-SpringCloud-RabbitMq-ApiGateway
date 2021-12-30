9468721206fce9485f6aaed40e70f4b5188d7860
GuilhermeJWT <guiromanno@gmail.com>
2021-12-29 21:41:00 -0300

    Finaliza classe de configuração do Spring Security

--- auth/src/main/java/br/com/systemsgs/security/WebSecurityConfig.java
+++ auth/src/main/java/br/com/systemsgs/security/WebSecurityConfig.java
@@ -1,21 +1,38 @@
 package br.com.systemsgs.security;
 
+import br.com.systemsgs.jwt.JWTTokenConfigure;
 import br.com.systemsgs.jwt.JWTTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final JWTTokenProvider tokenProvider;
+    private final JWTTokenProvider jwtTokenProvider;
 
     @Autowired
-    public WebSecurityConfig(JWTTokenProvider tokenProvider) {
-        this.tokenProvider = tokenProvider;
+    public WebSecurityConfig(JWTTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+             httpBasic().disable()
+             .csrf().disable()
+             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+             .authorizeRequests()
+             .antMatchers("/login").permitAll()
+             .anyRequest().authenticated()
+                .and()
+             .apply(new JWTTokenConfigure(jwtTokenProvider));
     }
 
     @Bean
